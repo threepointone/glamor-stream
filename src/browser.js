9a646b588ea3ab76dfe8ca6827e37eba3b1bace8
Sunil Pai <threepointone@fb.com>
2017-08-14 11:03:30 +0100

    node.remove

--- src/browser.js
+++ src/browser.js
@@ -5,7 +5,7 @@ export function hydrate(tree, element){
   let chunks = [...document.querySelectorAll('[data-glamor-chunk]')]
   chunks.forEach(node => document.head.appendChild(node))
   reactDOMHydrate(tree, element)
-  chunks.forEach(node => document.head.removeChild(node))
+  chunks.forEach(node => node.remove())
   chunks = null
 
 }
